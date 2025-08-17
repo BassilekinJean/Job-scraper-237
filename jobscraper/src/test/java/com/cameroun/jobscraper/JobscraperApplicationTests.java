cb9a9e86201e1a655e86f8a5ffa699c0d28aaa29
BassilekinJean <jean.bassilekin@facsciences-uy1.cm>
2025-08-17 15:19:29 +0100

    Ajout de la structure de base du projet Spring Boot avec configuration Maven, fichiers de wrapper, et fichiers de configuration pour les environnements de développement.

---
+++ jobscraper/src/test/java/com/cameroun/jobscraper/JobscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cameroun.jobscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
